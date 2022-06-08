a5dacb95c9bd451294cda63c238bbbdf7e5632f3
Rix34 <radoslaw.skoczek@microsoft.wsei.edu.pl>
2022-06-08 15:49:05 +0200

    Add LoginForm and Loading vievs

---
+++ AListAppUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace AListAppUI.ViewModels
+{
+    public class ShellViewModel : Conductor<Object>
+    {
+        LoginFormViewModel _loginVM;
+
+        public ShellViewModel (LoginFormViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItemAsync(loginVM);
+
+        }
+
+    }
+}
--- DatabaseList/dbo/Tables/AnimeBaseData.sql
+++ DatabaseList/dbo/Tables/AnimeBaseData.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[AnimeBaseData]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Image] IMAGE NOT NULL, 
+    [Image] IMAGE NULL , 
     [Tytuł] NVARCHAR(50) NOT NULL, 
     [GenresID] INT NOT NULL, 
     [DataWydania] DATE NOT NULL, 
